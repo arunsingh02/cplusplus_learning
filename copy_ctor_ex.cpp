f88f27acbb9cd6dd7dc707b1a7953ca23bf79fdc
Arun Singh <49477977+arunsingh02@users.noreply.github.com>
2022-01-06 00:48:36 +0530

    Create copy_ctor_ex.cpp
    Copy ctor example. Complex(const Complex& c) main line.

---
+++ copy_ctor_ex.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Complex {
+  double re_, im_;
+
+  public:
+    Complex(double re, double im):
+      re_(re), im_(im)
+      {
+        cout << "Standard Ctor : ";
+        print();
+      }
+    Complex(const Complex& c):
+      re_(c.re_), im_(c.im_)
+      {
+        cout << "Copy Ctor: ";
+        print();
+      }
+    ~Complex(){
+      cout << "Standard Dtor : ";
+      print();
+    }
+
+    void print(){
+      cout << "|" << re_ << "+j" << im_ << "| = " << norm() << endl;
+    }
+
+    double norm(){
+      return sqrt(re_*re_ + im_*im_);
+    }
+};
+
+void display(Complex c_obj){
+  cout << "Copy Ctor test :";
+  c_obj.print();
+}
+
+int main(int argc, char const *argv[]) {
+  Complex c(4.2, 5.3);
+  display(c);
+  return 0;
+}
+
+/*
+Standard Ctor : |4.2+j5.3| = 6.7624
+Copy Ctor: |4.2+j5.3| = 6.7624
+Copy Ctor test :|4.2+j5.3| = 6.7624
+Standard Dtor : |4.2+j5.3| = 6.7624
+Standard Dtor : |4.2+j5.3| = 6.7624
+*/
