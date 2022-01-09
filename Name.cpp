5928b949f28c703b6572b782b20d5c0e8b7dc2d9
Arun Singh <49477977+arunsingh02@users.noreply.github.com>
2022-01-10 03:17:42 +0530

    09_01_2022_added header file example

---
+++ Name.cpp
@@ -0,0 +1,22 @@
+#include "String.h"
+
+class Name {
+private:
+  String firstname, lastname;
+
+public:
+  Name (const char *f, const char *l):
+    firstname(f), lastname(l){}
+  virtual ~Name (){}
+  void print(){
+    cout << "My Name is ";
+    firstname.getName(); cout << " ";
+    lastname.getName(); cout  << "." << endl;
+  }
+};
+
+int main(int argc, char const *argv[]) {
+  Name name("Arun", "Singh");
+  name.print();
+  return 0;
+}
